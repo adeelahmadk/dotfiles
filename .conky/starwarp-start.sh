07206b57081fb5954de055198249b1b577055acc
Adeel Ahmad <cod3g3nki@users.noreply.github.com>
2019-12-18 18:35:51 +0800

    Add new conky conf: starwarp

---
+++ .conky/starwarp-start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+conky -p 2 -c "$HOME/.conky/starwarp/starwarp_sys" &
+conky -p 2 -c "$HOME/.conky/starwarp/starwarp_clock" &
+conky -p 2 -c "$HOME/.conky/starwarp/starwarp_os" &
+conky -p 2 -c "$HOME/.conky/starwarp/starwarp_geo" &
+#conky -p 2 -c "$HOME/.conky/starwarp/starwarp_news" &
+
