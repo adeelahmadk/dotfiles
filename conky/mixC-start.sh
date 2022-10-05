44f6c7bfd0dd3ce45022e809d535966e24cb759c
Adeel Ahmad <6880680+adeelahmadk@users.noreply.github.com>
2022-10-05 19:27:59 +0500

    feat(conky): add new starwarp config for weather forecast
    New config features live weather data (from OpenWeatherMap) and a 3-day
    forecast. all variables are exported to shell at startup.

--- conky/mixC-start.sh
+++ conky/mixC-start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+[ ! -d "$HOME/.cache/conky" ] && mkdir -p $HOME/.cache/conky
+[ -r "$HOME/storage/API/conky.env.sh" ] \
+    || { printf "Couldn't load env variables and API key.\nExport vars from a file and place its absolute address at line# ${LINENO} in `basename $0`\n"; exit 1; }
+source $HOME/storage/API/conky.env.sh
+
 conky -c "$HOME/.conky/mixC/conkyrc_mixC12" &
 conky -c "$HOME/.conky/mixC/conkyrc_mixC13" &
 conky -c "$HOME/.conky/mixC/conkyrc_mixC14" &
--- conky/starwarp-start.sh
+++ conky/starwarp-start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+[ ! -d "$HOME/.cache/conky" ] && mkdir -p $HOME/.cache/conky
+[ -r "$HOME/storage/API/conky.env.sh" ] \
+    || { printf "Couldn't load env variables and API key.\nExport vars from a file and place its absolute address at line# ${LINENO} in `basename $0`\n"; exit 1; }
 source $HOME/storage/API/conky.env.sh
+
 #conky -p 2 -c "$HOME/.config/conky/starwarp/starwarp_clock" &
 conky -p 2 -c "$HOME/.config/conky/starwarp/starwarp_weather" &
-conky -p 2 -c "$HOME/.config/conky/starwarp/starwarp_sys" &
-conky -p 2 -c "$HOME/.config/conky/starwarp/starwarp_os" &
-conky -p 2 -c "$HOME/.config/conky/starwarp/starwarp_geo" &
+#conky -p 2 -c "$HOME/.config/conky/starwarp/starwarp_sys" &
+#conky -p 2 -c "$HOME/.config/conky/starwarp/starwarp_os" &
+#conky -p 2 -c "$HOME/.config/conky/starwarp/starwarp_geo" &
 #conky -p 2 -c "$HOME/.conky/starwarp/starwarp_news" &
 
