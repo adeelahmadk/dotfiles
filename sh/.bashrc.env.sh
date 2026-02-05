921ee9778e077d680f13c4e8b678c9a739f9384b
Adeel Ahmad <6880680+adeelahmadk@users.noreply.github.com>
2026-02-05 23:38:49 +0500

    feat(sh): add color highlights
    Added color highlighting to `less` and `man` pagers, both in bash and
    zsh.

--- sh/.bashrc.env.sh
+++ sh/.bashrc.env.sh
@@ -1,26 +1,40 @@
-# Append paths
-export PATH=$PATH:/usr/local/go/bin
-
-# Set env vars
+## ---- setup env vars -------------------------------------
 export BASH_COMPLETION_USER_DIR="$HOME/.local/share/bash-completion/completions"
 export GOPATH="$HOME/.local/go"
 export GOBIN="$GOPATH/bin"
-export PATH=$PATH:$GOBIN
 
-## source scripts: activate apps, aliases, functions
+## ---- Append Paths ---------------------------------------
+export PATH=$PATH:/usr/local/go/bin:$GOBIN:$HOME/.cargo/bin
 
+[ -n $(command -v source-highlight) ] && {
+  # less syntax highlighting + source-highlight installation
+  export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+  export LESS=' -R'
+}
+[ -n $(command -v bat) ] && {
+  # manpage syntax highlighting
+  export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+  export MANROFFOPT="-c" # prevent garbled characters by disabling bold highlighting
+}
+
+## ---- initialize cli tools -------------------------------
+
+# source scripts: activate apps, aliases, functions
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 . "$HOME/.cargo/env"
 
+# node version manager
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
-eval "$(starship init bash)"
-eval "$(zoxide init bash)"
-
+# setup bash completions
 source "$BASH_COMPLETION_USER_DIR/alacritty.bash"
 source "$BASH_COMPLETION_USER_DIR/git-completion.bash"
 #source "$HOME/.bash-completion/git-completion.bash"
+
+# starship prompt,zoxide smart `cd`
+eval "$(starship init bash)"
+eval "$(zoxide init bash)"
 eval "$(uv generate-shell-completion bash)"
 eval "$(uvx generate-shell-completion bash)"
--- sh/.zshrc.env.zsh
+++ sh/.zshrc.env.zsh
@@ -11,6 +11,14 @@ export GOBIN="$GOPATH/bin"
 export PATH=$PATH:$GOBIN
 export VENV="$HOME/.local/share/venvs"
 export LIBVIRT_DEFAULT_URI='qemu:///system'
+
+[ -n $(command -v source-highlight) ] && {
+  # less syntax highlighting + source-highlight installation
+  export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s"
+  export LESS=' -R'
+}
+[ -n $(command -v bat) ] && { export MANPAGER="sh -c 'col -bx | bat -l man -p'"; export MANROFFOPT="-c"; }
+
 ## ---- initialize cli tools -------------------------------
 
 # source scripts: activate apps, aliases, functions
